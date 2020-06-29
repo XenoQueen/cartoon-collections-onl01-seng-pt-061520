de967278e166e7b96ed2c1fc380bff2fca3cdedd
Maika Kailani <queenmab051@gmail.com>
2020-06-29 22:07:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves(names)
+require 'pry'
 
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1)  do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet# code an argument here
